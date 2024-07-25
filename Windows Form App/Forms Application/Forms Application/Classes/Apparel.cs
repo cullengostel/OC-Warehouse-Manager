bf84e22870aa7c08267644ae52848355d06ad39f
Cullen G <gostelcm@dukes.jmu.edu>
2024-07-25 15:08:49 -0400

    Created Forms App
    Created Forms App and implemented Apparel class and ApparelType enum

---
+++ Windows Form App/Forms Application/Forms Application/Classes/Apparel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forms_Application.Classes
+{
+    //Author:  Cullen Gostel
+    //Version: 07/25/2024
+    public class Apparel
+    {
+        int ItemNumber; //Unique ID
+        ApparelType Type; //See ApparelType.cs
+        string Color;
+        string Size;
+        string Style; //Additional style info if needed
+        int Quantity;
+
+        public Apparel(int itemNumber, ApparelType type, string color, string size, string style, int quantity)
+        {
+            ItemNumber = itemNumber;
+            Type = type;
+            Color = color;
+            Size = size;
+            Style = style;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ Windows Form App/Forms Application/Forms Application/Classes/ApparelType.cs
@@ -0,0 +1,19 @@
+﻿namespace Forms_Application.Classes
+{
+    public enum ApparelType
+    {
+        LongSleeveShirt,
+        ShortSleeveShirt,
+        PocketedShirt,
+        TankTop,
+        Hoodie,
+        Sweatshirt,
+        PoloShirt,
+        TShirt,
+        Blouse,
+        CropTop,
+        TubeTop,
+        VNeckShirt,
+        Coozie
+    }
+}
