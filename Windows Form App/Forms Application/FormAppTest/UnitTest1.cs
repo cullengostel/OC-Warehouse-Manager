b605a05b27fb8cb6ee34172b0d18606cf258f35f
Cullen G <gostelcm@dukes.jmu.edu>
2024-07-31 12:29:01 -0400

    Restructured for Unit Testing
    Created a new project "FormAppTest
     within the Forms Application solution

---
+++ Windows Form App/Forms Application/FormAppTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FormAppTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
